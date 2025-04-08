90e046a5bc2a2a6d7e2a31f7baf9fc64c95bb9cf
Juan Antonio Martínez López <86200289+JuananMtez@users.noreply.github.com>
2025-04-08 12:59:02 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.demo;
 public class UserHelper {
 
     public static void doStuff(String n, int a) {
-            x = x + 2;
+            x = x + 2
             if (x == 4) break;
         }
         return x;
