585bd871e69cf8892ca435e5f5e799014705594c
Juan Antonio Martínez López <86200289+JuananMtez@users.noreply.github.com>
2025-04-08 12:14:10 +0200

    csadñcda

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,20 +3,6 @@ package com.example.demo;
 public class UserHelper {
 
     public static void doStuff(String n, int a) {
-        if (n != null && !n.equals("")) {
-            if (a > 18) {
-                System.out.println("Hello " + n + ", access granted.");
-            } else {
-                System.out.println("Nope.");
-            }
-        } else {
-            System.out.println("no name");
-        }
-    }
-
-    public static int getNum() {
-        int x = 0;
-        while (x < 10) {
             x = x + 2;
             if (x == 4) break;
         }
