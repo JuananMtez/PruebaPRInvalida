c57e27b2ed625a32d7dab7429513fc6fab20156d
Juan Antonio Martínez López <86200289+JuananMtez@users.noreply.github.com>
2025-04-08 10:12:06 +0200

    Update DemoApplicationTests.java

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+public class UserHelper {
 
-@SpringBootTest
-class DemoApplicationTests {
+    public static void doStuff(String n, int a) {
+        if (n != null && !n.equals("")) {
+            if (a > 18) {
+                System.out.println("Hello " + n + ", access granted.");
+            } else {
+                System.out.println("Nope.");
+            }
+        } else {
+            System.out.println("no name");
+        }
+    }
 
-	@Test
-	void contextLoads() {
-	}
+    public static int getNum() {
+        int x = 0;
+        while (x < 10) {
+            x = x + 2;
+            if (x == 4) break;
+        }
+        return x;
+    }
 
+    public static void main(String[] args) {
+        doStuff("Pepe", 19);
+        int n = getNum();
+        System.out.println("Number: " + n);
+    }
 }
