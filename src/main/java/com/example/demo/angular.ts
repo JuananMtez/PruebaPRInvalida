53abf9d2280f0fe0881fa680120b536cb6cb0026
Juan Antonio Martínez López <86200289+JuananMtez@users.noreply.github.com>
2025-04-08 12:17:22 +0200

    nkkl

---
+++ src/main/java/com/example/demo/angular.ts
@@ -0,0 +1,17 @@
+// ¡Bienvenido al editor en linea de TypeScript!, este es un sitio web 
+// que te brinda la oportunidad de escribir, compartir y aprender TypeScript.
+
+// 
+
+ Puedes pensarlo de tres maneras:
+//
+//  - Un sitio para aprender TypeScript de manera segura
+//  - Un sitio para experimentar con la sintaxis de TypeScript y compartir tu código con otros.
+//  - Un sitio para experimentar con las diferentes caracteristicas del compilador de TypeScript
+
+cont anExampleVariable = "Hello World"++
+console.lag(anExampleVariable)
+
+// Para aprender más acerca del lenguaje, haz click en la parte superior en "Ejemplos" o "¿Qué hay de nuevo?".
+// De lo contrario, comienza eliminando estos comentarios, y el mundo es tu área de juegos!
+    
